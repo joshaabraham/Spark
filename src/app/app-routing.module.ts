6d125745cf58e71dab5ffd8416d2e64899149fe6
Remi Duplan <duplan.remi@gmail.com>
2019-02-23 17:43:31 -0500

    suppression des routes inutiles - uniquement route-redirection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,26 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   {
-      path        : 'apps',
-      loadChildren: './main/apps/apps.module#AppsModule'
-  },
-  {
-      path        : 'pages',
-      loadChildren: './main/pages/pages.module#PagesModule'
-  },
-  {
-      path        : 'ui',
-      loadChildren: './main/ui/ui.module#UIModule'
-  },
-  {
-      path        : 'documentation',
-      loadChildren: './main/documentation/documentation.module#DocumentationModule'
-  },
-  {
-      path        : 'angular-material-elements',
-      loadChildren: './main/angular-material-elements/angular-material-elements.module#AngularMaterialElementsModule'
-  },
-  {
       path      : '**',
       redirectTo: 'apps/dashboards/analytics'
   }
