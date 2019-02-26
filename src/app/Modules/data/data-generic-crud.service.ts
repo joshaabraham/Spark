fed559649edd27fb622e4c9f358348da4d90155d
Remi Duplan <duplan.remi@gmail.com>
2019-02-26 17:15:36 -0500

    création du data-generic-crud.service.ts , pour faire les opérations sans se soucier des model d'objet.

---
+++ src/app/Modules/data/data-generic-crud.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataGenericCrudService {
+
+  endpoint: string = '[YOUR_DB_SERVER_IP]';
+
+  constructor(private http: HttpClient) {}
+
+  
+      // CREATE
+      create<T>(model: T | any, objToCreate: T | any): Observable<T | T[]> {
+        return this.http.post<T | T[]>(`${this.endpoint}/${model.tableName}`, objToCreate);
+      }
+
+      // READ
+      read<T>(model: T | any): Observable<T | T[]> {
+        return this.http.get<T | T[]>(`${this.endpoint}/${model.tableName}`);
+      }
+
+      // UPDATE
+      update<T>(model: T | any, objToUpdate: T | any): Observable<T | T[]> {
+        return this.http.patch<T | T[]>(`${this.endpoint}/${model.tableName}/${objToUpdate.id}`, objToUpdate); 
+      } 
+
+    
+      // DELETE 
+      delete<T>(model: T | any, objToDelete): Observable<T | T[]> {
+        return this.http.delete<T | T[]>(`${this.endpoint}/${model.tableName}/${objToDelete.id}`);
+      }
+  
+}
