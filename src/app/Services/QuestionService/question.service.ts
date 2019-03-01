56726e4b11468da74e8d53a1abb4b7cb27d34c06
Remi Duplan <duplan.remi@gmail.com>
2019-03-01 12:00:48 -0500

    Ajout d'un module et service de formulaire dynamique dans Modules/forms et Services/QuestionService

---
+++ src/app/Services/QuestionService/question.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+import { DropdownQuestion } from '../../Modules/forms/questionControls/controls/question-dropdown';
+import { QuestionBase } from '../../Modules/forms/questionControls/question-base';
+import { TextboxQuestion } from '../../Modules/forms/questionControls/controls/question-textbox';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+
+  constructor() { }
+}
