97d939708bf6ce47faab7a1d6e3dc6fddede8691
Remi Duplan <duplan.remi@gmail.com>
2019-03-01 13:23:44 -0500

    correction d'une erreure de lien relatif avec DropDownQuestion, et exemple d'utilisation du service dans QuestionService.ts

--- src/app/Services/QuestionService/question.service.ts
+++ src/app/Services/QuestionService/question.service.ts
@@ -9,5 +9,48 @@ import { TextboxQuestion } from '../../Modules/forms/questionControls/controls/q
 })
 export class QuestionService {
 
-  constructor() { }
+  // Pour voir commebnt utiliser ce service , suivre l'exemple de AppComponent.ts 
+  // ici : https://angular.io/guide/dynamic-form#question-model
+
+ // constructor() { }
+   // TODO: get from a remote source of question metadata
+  // TODO: make asynchronous
+  /*
+  getQuestions() {
+ 
+   
+    let questions: QuestionBase<any>[] = [
+ 
+      new DropdownQuestion({
+        key: 'brave',
+        label: 'Bravery Rating',
+        options: [
+          {key: 'solid',  value: 'Solid'},
+          {key: 'great',  value: 'Great'},
+          {key: 'good',   value: 'Good'},
+          {key: 'unproven', value: 'Unproven'}
+        ],
+        order: 3
+      }),
+ 
+      new TextboxQuestion({
+        key: 'firstName',
+        label: 'First name',
+        value: 'Bombasto',
+        required: true,
+        order: 1
+      }),
+ 
+      new TextboxQuestion({
+        key: 'emailAddress',
+        label: 'Email',
+        type: 'email',
+        order: 2
+      })
+    ];
+    
+ 
+    return questions.sort((a, b) => a.order - b.order);
+  }
+  */
 }
