88d0fc1c73746269eb39429575723901230fbfee
Remi Duplan <duplan.remi@gmail.com>
2019-03-10 00:15:48 -0400

    routage vers les pages effectue, sauf login qui doit faire l'objet d'une imple. differente.

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -8,6 +8,36 @@ export const navigation: FuseNavigation[] = [
         type     : 'group',
         children : [
             {
+                id       : 'profil',
+                title    : 'Profil',
+                translate: 'NAV.PROFIL',
+                type     : 'collapsable',
+                icon     : 'person',
+                children : [
+                    {
+                        id        : 'edition',
+                        title     : 'Edition',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/products',
+                        exactMatch: true
+                    },
+                    {
+                        id        : 'vieprivee',
+                        title     : 'Vie Privee',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/products/1/printed-dress',
+                        exactMatch: true
+                    },
+                    {
+                        id        : 'recherche',
+                        title     : 'Recherche',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/orders',
+                        exactMatch: true
+                    }
+                ]
+            },
+            {
                 id       : 'sample',
                 title    : 'Sample',
                 translate: 'NAV.SAMPLE.TITLE',
@@ -44,7 +74,44 @@ export const navigation: FuseNavigation[] = [
                 type     : 'item',
                 icon     : 'work',
                 url      : '/professionel',
-            }
+            },
+            {
+                id       : 'e-commerce',
+                title    : 'E-Commerce',
+                translate: 'NAV.ECOMMERCE',
+                type     : 'collapsable',
+                icon     : 'shopping_cart',
+                children : [
+                    {
+                        id        : 'products',
+                        title     : 'Products',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/products',
+                        exactMatch: true
+                    },
+                    {
+                        id        : 'productDetail',
+                        title     : 'Product Detail',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/products/1/printed-dress',
+                        exactMatch: true
+                    },
+                    {
+                        id        : 'orders',
+                        title     : 'Orders',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/orders',
+                        exactMatch: true
+                    },
+                    {
+                        id        : 'orderDetail',
+                        title     : 'Order Detail',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/orders/1',
+                        exactMatch: true
+                    }
+                ]
+            },
         ]
     }
 ];
